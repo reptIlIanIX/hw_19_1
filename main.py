95e192df8f4b3663cfefce03b867f309cb84225e
Denis_b <spawnslash@mail.ru>
2023-08-02 16:58:31 +0300

    hw_1 done

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-<!doctype html>
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import codecs
+
+
+# Для начала определим настройки запуска
+hostName = "localhost" # Адрес для доступа по сети
+serverPort = 8080 # Порт для доступа по сети
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+    def get_html(self):
+        return """<!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -360,4 +374,28 @@
     })()
 </script>
 </body>
-</html>
+</html>"""
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        self.send_response(200) # Отправка кода ответа
+        page_content = self.get_html()
+        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
+        self.end_headers() # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, 'utf-8')) # Тело ответа
+
+if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
